02c3a486d4275581a1be080b7e6137c5114271ca
Cedric-Fournier <cedricfournier.pro@gmail.com>
2018-06-19 16:13:19 +0200

    Update login-register

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,13 +1,20 @@
 // LOGIN - REGISTER //
 $(document).ready(function(){
     $("#mySignIn").click(function(){
-        $("#myModalSignIn").modal();
+      $("#myModalSignIn").modal();
     });
 });
 
 $(document).ready(function(){
     $("#mySignUp").click(function(){
-        $("#myModalSignUp").modal();
+      $("#myModalSignUp").modal();
     });
 });
+
+// $(document).ready(function(){
+//     $("#test").click(function(){
+//       $("#myModalSignIn").popup( "close" );
+//       $("#myModalSignUp").modal();
+//     });
+// });
 // LOGIN - REGISTER //
