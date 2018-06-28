7d554604e86193f292f835e9aaf6ceb9920a5f58
Cedric-Fournier <cedricfournier.pro@gmail.com>
2018-06-28 14:23:16 +0200

    Codage Finish

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -48,6 +48,121 @@ $('.launch-modal').on('click', function(e){
 
 
 // MODAL MOVIES //
+// FEATURED MOVIES //
+
+function Shuffle(o) {
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
+
+function showMovies(data) {
+  let img = data[i].url;
+  let year = data[i].date;
+  let title = data[i].name;
+  let y = data.indexOf(data[i]);
+  let entry = '<div class="card col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12"><img class="card-img-top" src=' + img + ' alt=' + title + '><div class="card-footer"><div class="text-center txt1 filmouf">' + title + '</div><br><div class=txt1>' + year + '</div></div></div>';
+  if(y > 11) {
+    $(entry).appendTo($('.wrapperJson3'));
+  }
+  else if(y > 5) {
+    $(entry).appendTo($('.wrapperJson2'));
+  } else {
+    $(entry).appendTo($('.wrapperJson'));
+  }
+}
+
+function clearMovies() {
+  $(".wrapperJson").empty();
+  $(".wrapperJson2").empty();
+  $(".wrapperJson3").empty();
+}
+
+function moviesSelect(json) {
+  clearMovies();
+  $.getJSON(json, function(data){
+    for(i in data) {
+      showMovies(data);
+    }
+  });
+}
+
+function allMovies() {
+  clearMovies();
+  $.getJSON("./Assets/bibliotheque/JSON/movies.json", function(data){
+    let test = Shuffle(data);
+    for(i in data) {
+      showMovies(data);
+    }
+  });
+}
+
+// FEATURED MOVIES //
+// SHOP MOVIE //
+
+function shop(num1, num2) {
+  $.getJSON('./Assets/bibliotheque/JSON/movies.json', function(data){
+    for(i = num1; i <= num2; i++){
+      let img = data[i].url;
+      let title = data[i].name;
+      let year = data[i].date;
+      let price = data[i].price;
+      let id = 'movieID' + i
+      let entry = '<div class="col-12 col-md-3 col-sm-6 col-lg-3"><div class="card imgclick" id=' + id + '><img class="card-img-top imgclick2" src=' + img + ' alt=' + title + '><div class="card-footer"><div class="text-center txt1 filmouf">' + title + '</div><br><div class="row"><div class="col txt1">' + year + '</div><div class="col txt1 prix">' + price + '</div></div></div></div></div>';
+      if(i % 2 === 0){
+        $(entry).appendTo($('.wrapperShop1'));
+      } else {
+        $(entry).appendTo($('.wrapperShop2'));
+      }
+    }
+  });
+}
+
+shop(0, 7);
+let x = 0;
+let y = 7;
+$('#shopForward').click(function(){
+  $(".wrapperShop1").empty();
+  $(".wrapperShop2").empty();
+  x += 8;
+  y += 8;
+  if(x<24){
+    shop(x, y);
+    $('#shopBack').removeAttr("disabled");
+  } else {
+    shop(24, 30);
+    $(this).attr("disabled", "true");
+  }
+});
+
+$('#shopBack').click(function(){
+  $(".wrapperShop1").empty();
+  $(".wrapperShop2").empty();
+  x -= 8;
+  y -= 8;
+  if(x <= 0) {
+    $(this).attr("disabled", "true");
+    shop(0, 7);
+  } else {
+    shop(x, y);
+    $('#shopForward').removeAttr("disabled");
+  }
+});
+
+$(document).on("click", ".imgclick", function(){
+  let source = $(this).attr("id");
+  let index2 = Number(source.slice(7));
+  $.getJSON('./Assets/bibliotheque/JSON/movies.json', function(data){
+    let trailer = data[index2].trailer;
+    $('#embed1').attr('src', trailer);
+    $('#embed2').html(data[index2].name);
+    $('#embed3').html(data[index2].storyline);
+    $('#embed4').html(data[index2].date);
+    $('#embed5').html(data[index2].genre);
+    $('#embed6').html(data[index2].price);
+  });
+});
+
+// SHOP MOVIE //
 // CONTACT //
 
 let submitContact = () => {
@@ -70,76 +185,6 @@ function GoogleMap() {
 
 
 // CONTACT //
-// SHOP MOVIE //
-
-function shop(num1, num2) {
-  $.getJSON('./Assets/bibliotheque/JSON/movies.json', function(data){
-    for(i = num1; i <= num2; i++){
-      let img = data[i].url;
-      let title = data[i].name;
-      let year = data[i].date;
-      let price = data[i].price;
-      let id = 'movieID' + i
-      let entry = '<div class="col-12 col-md-3 col-sm-6 col-lg-3"><div class="card imgclick" id=' + id + '><img class="card-img-top imgclick2" src=' + img + ' alt=' + title + '><div class="card-footer"><div class="text-center txt1 filmouf">' + title + '</div><br><div class="row"><div class="col txt1">' + year + '</div><div class="col txt1 prix">' + price + '</div></div></div></div></div>';
-      if(i % 2 === 0){
-        $(entry).appendTo($('.wrapperShop1'));
-      } else {
-        $(entry).appendTo($('.wrapperShop2'));
-      }
-    }
-  });
-}
-
-//show more or less movies on shop section
-
-shop(0, 7);
-let x = 0;
-let y = 7;
-$('#shopForward').click(function(){
-  $(".wrapperShop1").empty();
-  $(".wrapperShop2").empty();
-  x += 8;
-  y += 8;
-  if(x<32){
-    shop(x, y);
-    $('#shopBack').removeAttr("disabled");
-  } else {
-    shop(32, 39);
-    $(this).attr("disabled", "true");
-  }
-});
-
-$('#shopBack').click(function(){
-  $(".wrapperShop1").empty();
-  $(".wrapperShop2").empty();
-  x -= 8;
-  y -= 8;
-  if(x <= 0) {
-    $(this).attr("disabled", "true");
-    shop(0, 7);
-  } else {
-    shop(x, y);
-    $('#shopForward').removeAttr("disabled");
-  }
-});
-
-//get trailer for card in shop section
-
-$(document).on("click", ".imgclick", function(){
-  let source = $(this).attr("id");
-  let index2 = Number(source.slice(7));
-  $.getJSON('./Assets/bibliotheque/JSON/movies.json', function(data){
-    let trailer = data[index2].trailer;
-    $('#embed1').attr('src', trailer);
-    $('#embed2').html(data[index2].name);
-    $('#embed3').html(data[index2].storyline);
-    $('#embed4').html(data[index2].date);
-    $('#embed5').html(data[index2].genre);
-    $('#embed6').html(data[index2].price);
-  });
-});
-
-// SHOP MOVIE //
 // BUTTON UP //
 
 let buttonUp = () =>{
@@ -172,77 +217,29 @@ $(document).ready(function(){
   GoogleMap();
   //ageVerif();
   // buttonUp();
-  shop();
+  allMovies();
+  $("#actionButton").click(function(){
+    moviesSelect("./Assets/bibliotheque/JSON/action.json");
+  });
+  $("#comedieButton").click(function(){
+    moviesSelect("./Assets/bibliotheque/JSON/comedie.json");
+  });
+  $("#scifiButton").click(function(){
+    moviesSelect("./Assets/bibliotheque/JSON/scifi.json");
+  });
+  $("#allButton").click(function(){
+    allMovies();
+  });
+  $("#theshowmust").click(function(){
+  let hideshow = $(".hiddenRow").css("display");
+  $(".showmore, .showless").toggle(function(){
+    if(hideshow == "none") {
+      $('.hiddenRow').css("display", "flex");
+    } else {
+      $('.hiddenRow').css("display", "none");
+    }
+  });
+});
 });
 
 // LAUNCHER //
-=======
-// MODAL FILMS //
-
-// FILMS SELECTIONS //
-/*
-$('.btn1').click(function(){
-    $(this).text(function(i,old){
-        return old=='PLUS DE FILMS' ?  'MOINS DE FILMS' : 'PLUS DE FILMS';
-    });
-}); */
-
-// FILMS SELECTIONS //
-
-// FILMS FILTER //
-
-$("#showall").on("click", function(){
-    $("#b1, #b2, #b3").addClass("cachee");
-    $("#c1, #c2, #c3").addClass("cachee");
-    $("#a1, #a2").removeClass("cachee");
-});
-
-$("#showaction").on("click", function(){
-    $("#a1, #a2, #a3").addClass("cachee");
-    $("#c1, #c2, #c3").addClass("cachee");
-    $("#b1, #b2").removeClass("cachee");
-});
-
-$("#showaventure").on("click", function(){
-    $("#a1, #a2, #a3").addClass("cachee");
-    $("#b1, #b2, #b3").addClass("cachee");
-    $("#c1, #c2").removeClass("cachee");
-});
-
-$("#showmore").on("click", function(){
-    if($("#b1").attr("class") == "row cachee" && $("#c1").attr("class") == "row cachee"){
-        $("#a3").removeClass('cachee');
-    }
-    else if($("#a1").attr("class") == "row cachee" && $("#c1").attr("class") == "row cachee"){
-        $("#b3").removeClass('cachee');
-    }
-    else {
-        $("#c3").removeClass('cachee');
-    }
-    $('#showless').removeClass('cachee');
-    $('#showmore').addClass('cachee');
-});
-
-$("#showless").on("click", function(){
-    if($('#a3').attr('class') == "row"){
-        $('#a3').addClass("cachee");
-    }
-    else if($('#b3').attr('class') == "row"){
-        $('#b3').addClass("cachee");
-    }
-    else {
-        $('#c3').addClass("cachee");
-    }
-    $('#showmore').removeClass('cachee');
-    $('#showless').addClass('cachee');
-});
-/*$(function() {
-    $('.post').hide();
-    $("#filter button").on("click", function(){
-        var filtertag = $(this).attr('class');
-        $('.post').hide().filter('.' + filtertag).show();
-    })
-        [0].click();
-});*/
-
-// FILMS FILTER //
